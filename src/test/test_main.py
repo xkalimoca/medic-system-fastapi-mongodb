ba4ca7ea367acdb65c2145e3f2e16d08366f6fbc
xkalimoca <klmc_02@hotmail.com>
2023-07-29 17:00:25 -0500

    changes2.1

--- src/test/test_main.py
+++ src/test/test_main.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 
 
 def test_read_item():
-    response = client.get("/items/foo", headers={"X-Token": "llosavargasmario"})
+    response = client.get("/citas/foo", headers={"X-Token": "llosavargasmario"})
     assert response.status_code == 200
     assert response.json() == {
         "id": "foo",
